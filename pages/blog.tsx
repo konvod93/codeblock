c11cbc24c4dcc8922d41546cf781b29f26606d00
Konstantin <konvod93@gmail.com>
2022-10-23 20:35:12 +0300

    added sorting posts

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,34 +1,46 @@
 import CardComponent from "../components/Card.component";
 import { Post } from "../models/Post.interface";
-import { GetStaticProps } from "next";
+import { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
 import fs from 'fs';
 import path from "path";
 import matter from "gray-matter";
+import { Typography } from "@mui/material";
 
-const Blog = () => {
-    return (
-    <div>
+const Blog: NextPage = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  return (
+    <>
+    <Typography align='center' color='primary' variant='h1'>Blog</Typography>
+      <div style={{ display: 'flex' }}>
         {posts.map((post: Post, index: number) => (
           <CardComponent key={index} post={post} />
         ))}
-    </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default Blog
 
 export const getStaticProps: GetStaticProps = async () => {
-    const articlesDirectory = path.join('articles');
-  
-    const files = fs.readdirSync(articlesDirectory);
-  
-    const blogPosts = files.map((fileName: string) => {
-      const slug = fileName.replace('.mdx', '');
-  
-      const article = fs.readFileSync(path.join('articles', fileName));
-      const { data: metaData } = matter(article);
-      return { slug, metaData }
-    })
-   
-    return { props: { posts: blogPosts } };
-  }
+  const articlesDirectory = path.join('articles');
+
+  const files = fs.readdirSync(articlesDirectory);
+
+  const blogPosts: Post[] = files.map((fileName: string) => {
+    const slug = fileName.replace('.mdx', '');
+
+    const article = fs.readFileSync(path.join('articles', fileName));
+    const { data: metaData } = matter(article);
+    return { slug, metaData } as Post;
+  })
+
+  return {
+    props: {
+      posts:
+        blogPosts.sort((a: Post, b: Post) =>
+          new Date(b.metaData.dateString).valueOf() -
+          new Date(a.metaData.dateString).valueOf()
+        ),
+    },
+  };
+};
