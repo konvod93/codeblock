7b2d9855cbc092d56ee78ef08e6805ffcd1b0652
Konstantin <konvod93@gmail.com>
2022-10-23 12:06:09 +0300

    displaying tags

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,5 +1,9 @@
 import CardComponent from "../components/Card.component";
 import { Post } from "../models/Post.interface";
+import { GetStaticProps } from "next";
+import fs from 'fs';
+import path from "path";
+import matter from "gray-matter";
 
 const Blog = () => {
     return (
@@ -12,3 +16,19 @@ const Blog = () => {
 };
 
 export default Blog
+
+export const getStaticProps: GetStaticProps = async () => {
+    const articlesDirectory = path.join('articles');
+  
+    const files = fs.readdirSync(articlesDirectory);
+  
+    const blogPosts = files.map((fileName: string) => {
+      const slug = fileName.replace('.mdx', '');
+  
+      const article = fs.readFileSync(path.join('articles', fileName));
+      const { data: metaData } = matter(article);
+      return { slug, metaData }
+    })
+   
+    return { props: { posts: blogPosts } };
+  }
